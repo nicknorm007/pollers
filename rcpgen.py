f0f6ea84655d12163e6513f4a90fe7ec7e029af2
Nick Norman <nicknorm007@gmail.com>
2020-06-21 10:41:25 -0400

    add class

--- rcpgen.py
+++ rcpgen.py
@@ -2,4 +2,5 @@ from rcppoller import RcpPoller
 
 poller = RcpPoller('data/polldata.csv')
 poller.readPollingDataUrls()
+poller.iterateStates()
 
--- rcppoller.py
+++ rcppoller.py
@@ -8,6 +8,18 @@ class RcpPoller:
         self.poll_data = []
         self.poll_map = {}
 
+    def __pointSpread(self,elem):
+        avrge = elem.find('td', class_='spread')
+        return avrge.text
+
+    def __findAverages(self,res):
+        avg_poll_elems = res.find_all('tr', class_='rcpAvg')
+        poll_elems = res.find_all('tr', class_='isInRcpAvg')
+        for poll_elem in avg_poll_elems:
+            print("Average: ", self.__pointSpread(poll_elem))
+        for poll_elem in poll_elems:
+            print(self.__pointSpread(poll_elem))
+
     def readPollingDataUrls(self):
         with open(self.dataFile, newline='') as csvfile:
             self.poll_data = csv.reader(csvfile)
@@ -15,24 +27,12 @@ class RcpPoller:
                 self.poll_map[ row[0] ] = row[1]
         print(self.poll_map)
 
-    def pointSpread(self,elem):
-        avrge = elem.find('td', class_='spread')
-        return avrge.text
-
-    def findAverages(self,res):
-        avg_poll_elems = res.find_all('tr', class_='rcpAvg')
-        poll_elems = res.find_all('tr', class_='isInRcpAvg')
-        for poll_elem in avg_poll_elems:
-            print("Average: ", pointSpread(poll_elem))
-        for poll_elem in poll_elems:
-            print(pointSpread(poll_elem))
-
     def iterateStates(self):
         for state,url in self.poll_map.items():
             page = requests.get(url)
             soup = BeautifulSoup(page.content, 'html.parser')
             results = soup.find(id='polling-data-rcp')
             print(state)
-            findAverages(results)
+            self.__findAverages(results)
     
         
